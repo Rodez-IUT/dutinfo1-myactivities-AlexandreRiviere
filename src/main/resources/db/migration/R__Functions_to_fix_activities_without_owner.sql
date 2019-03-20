213370b983a8f7b9ef0e5ea9ba4c6ecd5f1ab108
Alexandre Riviere <alexandre.riviere@iut-rodez.fr>
2019-03-20 15:47:28 +0100

    function to fix activities without owner

--- src/main/resources/db/migration/R__Functions_to_fix_activities_without_owner.sql
+++ src/main/resources/db/migration/R__Functions_to_fix_activities_without_owner.sql
@@ -1,16 +1,31 @@
-CREATE OR REPLACE function get_default_owner() returns "user" as $$
-declare
-    defaultOwner "user"%rowtype;
-    defaultOwnerUsername varchar(500) := 'Default Owner';
-begin
-	SELECT * INTO DefaultOwner from "user"
-	WHERE username = defaultOwnerUsername;
-	if not found then
-	    insert into "user"(id, username)
-	       values(nextval('id_generator'), defaultOwnerUsername);
-	    select * into defaultOwner from "user"
-	       where username = defaultOwnerUsername;
-	end if;
-	return defaultOwner;
-END
-$$ language plpgsql;
+CREATE OR REPLACE FUNCTION get_default_owner() RETURNS "user" AS $$
+    DECLARE
+        defaultOwner "user"%rowtype;
+        defaultOwnerUsername varchar(500) := 'Default Owner';
+    BEGIN
+        select * into defaultOwner from "user"
+            where username = defaultOwnerUsername;
+        if not found then
+                insert into "user" (id, username)
+                    values (nextval('id_generator'), defaultOwnerUsername);
+                select * into defaultOwner from "user"
+                    where username = defaultOwnerUsername;
+        end if;
+        return defaultOwner ;
+    END
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION fix_activities_without_owner() RETURNS SETOF activity AS $$
+    DECLARE
+        defaultOwner "user"%rowtype;
+    BEGIN
+        defaultOwner := get_default_owner();
+        return query
+        update activity
+        SET owner_id = defaultowner.id
+        where owner_id is null
+        returning * ;
+    END
+$$ LANGUAGE plpgsql;
